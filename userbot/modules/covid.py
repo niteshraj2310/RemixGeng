aae513c56864a8022f52c92e9ac34b4cba108e1c
niteshraj2310 <niteshraj02310@gmail.com>
2020-07-15 02:21:42 +0530

    noice

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -4,35 +4,55 @@
 # you may not use this file except in compliance with the License.
 #
 
-
-from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot.events import register
+from datetime import datetime
+from covid import Covid
 from userbot import CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern="^.covid (.*)")
+async def corona(event):
+    await event.edit("`Processing...`")
+    country = event.pattern_match.group(1)
+    covid = Covid(source="worldometers")
+    country_data = covid.get_status_by_country_name(country)
+    if country_data:
+        output_text =  f"`😷Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n"
+        output_text += f"`🤒Active      : {country_data['active']}`\n"
+        output_text += f"`🤕Critical    : {country_data['critical']}`\n"
+        output_text += f"`⚰Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n"
+        output_text += f"`😇Recovered   : {country_data['recovered']}`\n"
+        output_text += f"`🧪Total tests : {country_data['total_tests']}`\n"
+        output_text += f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
+    else:
+        output_text = "No information yet about this country!"
+
+    await event.edit(f"`Corona Virus Info in {country}:`\n\n{output_text}")
+
+@register(outgoing=True, pattern="^.covid$")
+async def corona(event):
+    await event.edit("`Processing...`")
+    country = "World"
+    covid = Covid(source="worldometers")
+    country_data = covid.get_status_by_country_name(country)
+    if country_data:
+        output_text =  f"`😷Confirmed   : {country_data['confirmed']} (+{country_data['new_cases']})`\n"
+        output_text += f"`🤒Active      : {country_data['active']}`\n"
+        output_text += f"`🤕Critical    : {country_data['critical']}`\n"
+        output_text += f"`⚰Deaths      : {country_data['deaths']} (+{country_data['new_deaths']})`\n"
+        output_text += f"`😇Recovered   : {country_data['recovered']}`\n"
+        output_text += f"`🧪Total tests : N/A`\n"
+        output_text += f"Data provided by [Worldometer](https://www.worldometers.info/coronavirus/country/{country})"
+    else:
+        output_text = "No information yet about this country!"
+
+    await event.edit(f"`Corona Virus Info in {country}:`\n\n{output_text}")
 
 
-@register(outgoing=True, pattern="^.corona (.*)")
-async def _(event):
-    if event.fwd_from:
-        return 
-    input_str = event.pattern_match.group(1)
-    reply_message = await event.get_reply_message()
-    chat = "@NovelCoronaBot"
-    await event.edit("```Checking...```")
-    async with event.client.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1124136160))
-              await event.client.send_message(chat, "{}".format(input_str))
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("```Unblock (@NovelCoronaBot)```")
-              return
-          if response.text.startswith("Country"):
-             await event.edit("😐**Country Not Found**😐")
-          else: 
-             await event.delete()
-             await event.client.send_message(event.chat_id, response.message)
+CMD_HELP.update({
+        "covid":
+        "`.covid `**<country>**"
+        "\n`Usage: Get an information about covid-19 data in your country.`\n\n"
+        "`.covid`"
+        "\n`Usage: Get an information about covid-19 data in Worldwide.`\n"
 
-CMD_HELP.update(
-    {"corona": ".corona [country]\n"
-     "Usage: Corona Virus stats."})
+    })
