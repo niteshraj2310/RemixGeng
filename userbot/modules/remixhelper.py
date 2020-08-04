38adb469eb9abbc6b02c32f51958803de5d0b8cd
Nitesh_XD <niteshraj2310@users.noreply.github.com>
2020-08-04 16:51:44 +0530

    just passing tym lmao ;__;
    Signed-off-by: Nitesh_XD <niteshraj2310@users.noreply.github.com>

--- userbot/modules/remixhelper.py
+++ userbot/modules/remixhelper.py
@@ -17,14 +17,14 @@ async def usit(e):
         "\n[Ubuntu Linux Method](https://telegra.ph/How-to-keep-OUB-repo-updated-while-keeping-your-changes-through-Ubuntu-Terminal-method-04-01-2)"
         "\n[Gdrive Tutorial](https://telegra.ph/How-To-Setup-Google-Drive-04-03)"
         "\n[video-tutorial](https://youtu.be/us1O-AnWmHA)")
-    
+
 @register(outgoing=True, pattern="^.remixvar$")
 async def var(m):
     await m.edit(
         f"Here's a list of VARS for {DEFAULTUSER} on **oub-remix**:\n"
-        "\n[HEROKU VARS](https://raw.githubusercontent.com/sahyam2019/oub-remix/sql-extended/varshelper.txt)")
-    
-    
+        "\n[HEROKU VARS](https://raw.githubusercontent.com/niteshraj2310/RemixGeng/sql-extended/varshelper.txt)")
+
+
 CMD_HELP.update({
     "remixhelper":
     "`.remixhelp`\
