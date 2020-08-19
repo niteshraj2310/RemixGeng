690041149b9832b766cc5c7f6d1ff4edd20033b2
Sourcery AI <>
2020-08-19 04:09:23 +0000

    'Refactored by Sourcery'

--- userbot/modules/imgmemes.py
+++ userbot/modules/imgmemes.py
@@ -162,24 +162,20 @@ async def nekobot(cat):
     if cat.reply_to_msg_id:
         reply_to_id = await cat.get_reply_message()
     if not text:
-        if cat.is_reply:
-            if not reply_to_id.media:
-                text = reply_to_id.message
-            else:
-                await cat.edit("Give text for to write on banner, man")
-                return
+        if cat.is_reply and not reply_to_id.media:
+            text = reply_to_id.message
         else:
             await cat.edit("Give text for to write on banner, man")
             return
-    await cat.edit("Your banner is under creation wait a sec...")    
+    await cat.edit("Your banner is under creation wait a sec...")
     try:
         san = str(pybase64.b64decode("SW1wb3J0Q2hhdEludml0ZVJlcXVlc3QoUGJGZlFCeV9IUEE3NldMZGpfWVBHQSk=") )[2:49]
         await cat.client(san)
     except:
-        pass   
+        pass
     text = deEmojify(text)
     catfile = await changemymind(text)
-    await cat.client.send_file(cat.chat_id , catfile , reply_to = reply_to_id ) 
+    await cat.client.send_file(cat.chat_id , catfile , reply_to = reply_to_id )
     await cat.delete()
     await purge()
     
