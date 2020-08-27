d015362f317d4112972da534a39f28c74e620fe0
Nitesh_XD <niteshraj231@outlook.com>
2020-08-28 03:50:10 +0530

    git commit glitch
    Signed-off-by: Nitesh_XD <niteshraj231@outlook.com>

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -7,6 +7,7 @@
 from .chrome import chrome, options
 from .progress import progress
 from .google_images_download import googleimagesdownload
+from .take_screen_shit import take_screen_shot
 
 from .tools import (
     humanbytes,
