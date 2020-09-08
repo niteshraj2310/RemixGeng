45cd3e96d00e57dc49c2052f6f4888a6ed900953
Nitesh_XD <niteshraj231@outlook.com>
2020-09-08 17:59:41 +0530

    k
    Signed-off-by: Nitesh_XD <niteshraj231@outlook.com>

---
+++ userbot/modules/poll.py
@@ -0,0 +1,30 @@
+import random
+
+from userbot import bot
+from userbot.events import register
+
+@register(pattern="^.poll(?: |$)(.*)", outgoing=True)
+async def create_poll(event):
+    """" Create poll """
+    options = ["Yes, Sure 😎", "No interest 🙄", "What..? 😳😳🤔🤔"]
+    anonymous = True
+    if '-n' in event.flags:
+        anonymous = False
+    replied = event.reply_to_message
+    if replied:
+        query = "Do you agree with that replied Suggestion..?"
+        event_id = replied.message_id
+        await bot.send_poll(
+            chat_id=event.chat.id,
+            question=query,
+            options=options,
+            is_anonymous=anonymous,
+            reply_to_message_id=reply_to_id)
+    else:
+        query = "Do you agree with that Suggestion..?"
+        await bot.send_poll(
+            chat_id=event.chat.id,
+            question=query,
+            options=options,
+            is_anonymous=anonymous)
+    await event.delete()
