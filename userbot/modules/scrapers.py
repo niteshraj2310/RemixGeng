d0bb18b0b3ca13bafad532554f2b3e3526021abc
Nitesh <niteshraj231@outlook.com>
2020-09-19 09:42:43 +0530

    emoji chamge
    Signed-off-by: Nitesh <niteshraj231@outlook.com>

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -31,6 +31,7 @@ from re import findall
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from urllib.parse import quote_plus
+from urllib.error import HTTPError
 from wikipedia import summary
 from wikipedia.exceptions import DisambiguationError, PageError
 from requests import get
@@ -294,17 +295,43 @@ async def wiki(wiki_q):
 
 
 @register(outgoing=True, pattern="^.ud (.*)")
-async def _(event):
-    if event.fwd_from:
-        return
-    await event.edit("processing...")
-    word = event.pattern_match.group(1)
-    urban = asyncurban.UrbanDictionary()
+async def urban_dict(ud_e):
+    """ For .ud command, fetch content from Urban Dictionary. """
+    await ud_e.edit("Processing...")
+    query = ud_e.pattern_match.group(1)
     try:
-        mean = await urban.get_word(word)
-        await event.edit("Text: **{}**\n\nMeaning: **{}**\n\nExample: __{}__".format(mean.word, mean.definition, mean.example))
-    except asyncurban.WordNotFoundError:
-        await event.edit("No result found for **" + word + "**")
+        define(query)
+    except HTTPError:
+        await ud_e.edit(f"Sorry, couldn't find any results for: {query}")
+        return
+    mean = define(query)
+    deflen = sum(len(i) for i in mean[0]["def"])
+    exalen = sum(len(i) for i in mean[0]["example"])
+    meanlen = deflen + exalen
+    if int(meanlen) >= 0:
+        if int(meanlen) >= 4096:
+            await ud_e.edit("`Output too large, sending as file.`")
+            file = open("output.txt", "w+")
+            file.write("Text: " + query + "\n\nMeaning: " + mean[0]["def"] +
+                       "\n\n" + "Example: \n" + mean[0]["example"])
+            file.close()
+            await ud_e.client.send_file(
+                ud_e.chat_id,
+                "output.txt",
+                caption="`Output was too large, sent it as a file.`")
+            if os.path.exists("output.txt"):
+                os.remove("output.txt")
+            await ud_e.delete()
+            return
+        await ud_e.edit("Text: **" + query + "**\n\nMeaning: **" +
+                        mean[0]["def"] + "**\n\n" + "Example: \n__" +
+                        mean[0]["example"] + "__")
+        if BOTLOG:
+            await ud_e.client.send_message(
+                BOTLOG_CHATID,
+                "ud query `" + query + "` executed successfully.")
+    else:
+        await ud_e.edit("No result found for **" + query + "**")
 
 
 @register(outgoing=True, pattern=r"^.voice(?: |$)([\s\S]*)")
