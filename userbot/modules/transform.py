99ab23e41f3fc7c221172873d525151bfffc45f6
Nitesh <niteshraj231@outlook.com>
2020-09-20 15:29:46 +0530

    modify
    Signed-off-by: Nitesh <niteshraj231@outlook.com>

--- userbot/modules/transform.py
+++ userbot/modules/transform.py
@@ -1,25 +1,12 @@
-
 # Authored by @Khrisna_Singhal
 # Ported from Userge by Alfiananda P.A
 
-import asyncio
-import io
 import os
-import random
-import re
-import textwrap
-import time
-from asyncio.exceptions import TimeoutError
-from random import randint, uniform
 
-from glitch_this import ImageGlitcher
-from PIL import Image, ImageDraw, ImageEnhance, ImageFont, ImageOps
-from telethon import events, functions, types
-from telethon.errors.rpcerrorlist import YouBlockedUserError
+from PIL import Image, ImageOps
 
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 from userbot.events import register
-from userbot.utils import progress
 from userbot.utils.tools import check_media
 
 Converted = TEMP_DOWNLOAD_DIRECTORY + "sticker.webp"
@@ -156,157 +143,6 @@ async def rotate(event):
     os.remove(Converted)
 
 
-@register(outgoing=True, pattern=r"^\.mmf(?: |$)(.*)")
-async def mim(event):
-    if not event.reply_to_msg_id:
-        await event.edit(
-            "`Syntax: reply to an image with .mmf` 'text on top' ; 'text on bottom' "
-        )
-        return
-
-    reply_message = await event.get_reply_message()
-    if not reply_message.media:
-        await event.edit("```reply to a image/sticker/gif```")
-        return
-    await bot.download_file(reply_message.media)
-    if event.is_reply:
-        data = await check_media(reply_message)
-        if isinstance(data, bool):
-            await event.edit("`Unsupported Files...`")
-            return
-
-        await event.edit(
-            "```Transfiguration Time! Mwahaha Memifying this image! (」ﾟﾛﾟ)｣ ```"
-        )
-        await asyncio.sleep(5)
-        text = event.pattern_match.group(1)
-        if event.reply_to_msg_id:
-            file_name = "meme.jpg"
-            to_download_directory = TEMP_DOWNLOAD_DIRECTORY
-            downloaded_file_name = os.path.join(
-                to_download_directory, file_name)
-            downloaded_file_name = await bot.download_media(
-                reply_message,
-                downloaded_file_name,
-            )
-            dls_loc = downloaded_file_name
-        webp_file = await draw_meme_text(dls_loc, text)
-        await event.client.send_file(
-            event.chat_id, webp_file, reply_to=event.reply_to_msg_id
-        )
-        await event.delete()
-        os.remove(webp_file)
-        os.remove(dls_loc)
-
-
-async def draw_meme_text(image_path, text):
-    img = Image.open(image_path)
-    os.remove(image_path)
-    i_width, i_height = img.size
-    m_font = ImageFont.truetype(
-        "resources/MutantAcademyStyle.ttf", int((70 / 640) * i_width)
-    )
-    if ";" in text:
-        upper_text, lower_text = text.split(";")
-    else:
-        upper_text = text
-        lower_text = ""
-    draw = ImageDraw.Draw(img)
-    current_h, pad = 10, 5
-    if upper_text:
-        for u_text in textwrap.wrap(upper_text, width=15):
-            u_width, u_height = draw.textsize(u_text, font=m_font)
-
-            draw.text(
-                xy=(((i_width - u_width) / 2) - 1, int((current_h / 640) * i_width)),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(((i_width - u_width) / 2) + 1, int((current_h / 640) * i_width)),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 1),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 1),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-
-            draw.text(
-                xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
-                text=u_text,
-                font=m_font,
-                fill=(255, 255, 255),
-            )
-            current_h += u_height + pad
-    if lower_text:
-        for l_text in textwrap.wrap(lower_text, width=15):
-            u_width, u_height = draw.textsize(l_text, font=m_font)
-
-            draw.text(
-                xy=(
-                    ((i_width - u_width) / 2) - 1,
-                    i_height - u_height - int((20 / 640) * i_width),
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(
-                    ((i_width - u_width) / 2) + 1,
-                    i_height - u_height - int((20 / 640) * i_width),
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(
-                    (i_width - u_width) / 2,
-                    (i_height - u_height - int((20 / 640) * i_width)) - 1,
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(
-                    (i_width - u_width) / 2,
-                    (i_height - u_height - int((20 / 640) * i_width)) + 1,
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-
-            draw.text(
-                xy=(
-                    (i_width - u_width) / 2,
-                    i_height - u_height - int((20 / 640) * i_width),
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(255, 255, 255),
-            )
-            current_h += u_height + pad
-
-    image_name = "memify.webp"
-    webp_file = os.path.join(TEMP_DOWNLOAD_DIRECTORY, image_name)
-    img.save(webp_file, "WebP")
-    return webp_file
-
-
 CMD_HELP.update(
     {
         "transform": ">`.ghost`"
@@ -317,12 +153,5 @@ CMD_HELP.update(
         "\nUsage: To mirror your image"
         "\n\n>`.rotate <value>`"
         "\nUsage: To rotate your image\n* The value is range 1-360 if not it'll give default value which is 90"
-    }
-)
-
-CMD_HELP.update(
-    {
-        "memify": ">`.mmf texttop ; textbottom`"
-        "\nUsage: Reply a sticker/image/gif and send with cmd."
     }
 )
