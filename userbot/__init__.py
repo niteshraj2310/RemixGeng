3d24ddf7917d04b84864dd63ff95a84dd7f9d6dc
Nitesh <niteshraj231@outlook.com>
2020-09-23 09:12:19 +0530

    Userbot: Changed Dockerfile from alpine to ubuntu
    Signed-off-by: Nitesh <niteshraj231@outlook.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2020 The Raphielscape Company LLC.
 #
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 # inline credit @keselekpermen69
 import sys
-""" Userbot initialization. """
+""" Userbot initialization."""
 
 import os
 import re
