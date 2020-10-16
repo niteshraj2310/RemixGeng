8af20d98958ac94e48a0be970d086c754701935e
Nitesh <niteshraj231@outlook.com>
2020-10-16 16:08:03 +0530

    tryin
    Signed-off-by: Nitesh <niteshraj231@outlook.com>

--- userbot/modules/test.py
+++ userbot/modules/test.py
@@ -8,7 +8,22 @@ from userbot import bot
 from userbot import TEMP_DOWNLOAD_DIRECTORY
 from userbot.events import register
 from userbot.utils import progress
+from PyDictionary import PyDictionary
 
+@register(outgoing=True, pattern=r"^\.meaning(?: |$)(.*)")
+async def _(event):
+    word = event.pattern_match.group(1)
+    dictionary = PyDictionary()
+    words = dictionary.meaning(word)
+    output = f"**Word :** __{word}__\n\n"
+    try:
+        for a, b in words.items():
+            output += f"**{a}**\n"
+            for i in b:
+                output += f"☞__{i}__\n"
+        await event.edit(output)
+    except Exception:
+        await event.edit(f"Couldn't fetch meaning of {word}")
 
 @register(outgoing=True, pattern=r"^\.imgs(?: |$)(.*)")
 async def _(event):
