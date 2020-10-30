941699a77868e87f8e8f88568c768779bb9dae8e
Nitesh <niteshraj231@outlook.com>
2020-10-31 00:30:43 +0530

    module to find sticker packs
    Co-Authored-By: sandeep.n <58665444+sandy1709@users.noreply.github.com>
    Co-Authored-By: abhinav6497 <6497.abhi@gmail.com>
    Co-Authored-By: Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
    Signed-off-by: Nitesh <niteshraj231@outlook.com>

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -18,14 +18,15 @@ from telethon.tl.types import (
     InputPeerNotifySettings,
     InputStickerSetID,
 )
-
+import requests
+from bs4 import BeautifulSoup as bs
 from userbot import CMD_HELP, bot
 from userbot.events import register
 
 PACK_FULL = "Whoa! That's probably enough stickers for one pack, give it a break. \
 A pack can't have more than 120 stickers at the moment."
 PACK_DOESNT_EXIST = "  A <strong>Telegram</strong> user has created the <strong>Sticker&nbsp;Set</strong>."
-
+combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
 @register(outgoing=True, pattern="^.kang($| )?((?![0-9]).+?)? ?([0-9]*)?")
 async def kang(event):
@@ -316,6 +317,27 @@ async def get_pack_info(event):
 
     await event.edit(OUTPUT)
 
+@register(outgoing=True, pattern=r"^\.stickers ?(.*)")
+async def cb_sticker(event):
+    split = event.pattern_match.group(1)
+    if not split:
+        await event.edit("`Provide some name to search for pack.`")
+        return
+    await event.edit("`Searching sticker packs`")
+    text = requests.get(combot_stickers_url + split).text
+    soup = bs(text, "lxml")
+    results = soup.find_all("div", {'class': "sticker-pack__header"})
+    if not results:
+        await event.edit("`No results found :(.`")
+        return
+    reply = f"**Sticker packs found for {split} are :**"
+    for pack in results:
+        if pack.button:
+            packtitle = (pack.find("div", "sticker-pack__title")).get_text()
+            packlink = (pack.a).get('href')
+            packid = (pack.button).get('data-popup')
+            reply += f"\n **• ID: **`{packid}`\n [{packtitle}]({packlink})"
+    await event.edit(reply)
 
 @register(outgoing=True, pattern="^.getsticker$")
 async def sticker_to_png(sticker):
