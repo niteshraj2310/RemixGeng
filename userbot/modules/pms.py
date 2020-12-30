2503a6462b8118826522eeb434fc9c97a83b0e8b
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-12-30 20:58:31 +0530

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -309,7 +309,7 @@ async def monito_p_m_s(event):
             if event.chat_id and NC_LOG_P_M_S:
                     await event.client.send_message(
                         PM_LOGGR_BOT_API_ID,
-                        "#Conversation\n" + "With " +
+                        "#" +
                         f"[{chat.first_name}](tg://user?id={chat.id})",
                     )
                 
