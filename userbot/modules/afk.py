eb03195aea5dce9c0e4f4c2b5a6302dd4775bfc4
Nitesh <niteshraj231@outlook.com>
2021-01-14 14:07:35 +0000

    pylint: auto fixes
    Signed-off-by: Nitesh <niteshraj231@outlook.com>

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -12,7 +12,7 @@ from random import choice, randint
 from telethon.events import StopPropagation
 
 from userbot import PM_AUTO_BAN  # pylint: disable=unused-imports
-from userbot import AFKREASON, BOTLOG, BOTLOG_CHATID, CMD_HELP, COUNT_MSG, ISAFK, USERS
+from userbot import AFKREASON, BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 
 # ========================= CONSTANTS ============================
